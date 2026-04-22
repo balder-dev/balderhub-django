baba7518b42444b0361ea3ddcaf163bfb0c2570c
matosys <max@matosys.de>
2026-04-22 10:55:33 +0200

    tests(app/bookstore): reconfigure admin to return id and use field structuring

--- tests/app/bookstore/book/admin.py
+++ tests/app/bookstore/book/admin.py
@@ -5,19 +5,28 @@ from .models import Author, Category, Book
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ['last_name', 'first_name', 'date_of_birth', 'date_of_death']
+    list_display = ['id', 'last_name', 'first_name', 'date_of_birth', 'date_of_death']
     search_fields = ['first_name', 'last_name']
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['id', 'name']
     search_fields = ['name']
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'isbn', 'publication_date', 'price']
+    list_display = ['id', 'title', 'author', 'isbn', 'publication_date', 'price']
     list_filter = ['categories', 'author', 'publication_date']
     search_fields = ['title', 'isbn', 'author__first_name', 'author__last_name']
     filter_horizontal = ['categories']
+
+    fields = (
+        ('title', ),
+        ('author',),
+        ('categories',),
+        ('price', 'publication_date', 'isbn'),
+        ('pages', ),
+        ('summary', )
+    )
