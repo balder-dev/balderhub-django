3af15cad474f9a494fcbf87059bd83203abe408f
matosys <max@matosys.de>
2026-04-22 10:56:19 +0200

    tests(app/bookstore): change default language code to GB

--- tests/app/bookstore/bookstore/settings.py
+++ tests/app/bookstore/bookstore/settings.py
@@ -103,14 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
