78f4d34fe8ae270e519d4ee0d7e85e2b36cd1cf9
matosys <max@matosys.de>
2026-07-10 16:54:34 +0200

    feat(utils/change_form_fields): support django 6.0 version for determining label for `M2MChangeFormField`
    is placed within `<legend>`

--- src/balderhub/django/lib/utils/gui/admin/change_form_fields/m2m_change_form_field.py
+++ src/balderhub/django/lib/utils/gui/admin/change_form_fields/m2m_change_form_field.py
@@ -8,6 +8,39 @@ class M2MChangeFormField(BaseChangeFormField):
     """Field container for many-to-many selector fields in a Django admin change form."""
 
     @property
+    def label(self):
+        """Returns the label element for this field."""
+        # TODO optimize
+        # workaround for django 6: label is placed on higher element:
+        #  <div class="form-row field-categories">
+        #   <fieldset aria-describedby="id_categories_helptext">
+        #       <legend for="id_categories">Categories:</legend>
+        #       <div>
+        #           <div class="flex-container">
+        #               <div class="related-widget-wrapper" data-model-ref="category">
+        #                   <div class="selector">
+        #                       <div class="selector-available">
+        #                           ...
+        #                       </div>
+        #                       <ul class="selector-chooser">
+        #                           ...
+        #                       </ul>
+        #                       <div id="id_categories_selector_chosen" class="selector-chosen">
+        #                           ...
+        #                       </div>
+        #                       ...
+        #                   </div>
+        #               </div>
+        #           </div>
+        #       </div>
+        #  </fieldset>
+        # </div>
+        legend = html.HtmlElement.by_selector(self.driver, Selector.by_xpath('../../legend'), parent=self)
+        if legend.exists():
+            return legend
+        return super().label
+
+    @property
     def field(self) -> ManyToManySelectorWidget:
         """Returns the many-to-many selector widget element."""
         return ManyToManySelectorWidget.by_selector(
